5bfac9572ca998a5753deb45fa6e793688edede6
Rybots <tokorotenpi2bagu@gmail.com>
2016-03-19 02:48:16 +0900

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,21 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Rubitter"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Rubitter"
+  end
+
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | Rubitter"
+ 
+
   end
 
 end
